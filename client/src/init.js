faca5a39d672eb1731f10a3bd16ebd21e69fa7a7
Haneefa Shaik <haneefa506@gmail.com>
2022-08-24 19:53:54 +0200

    Adding html and css files

---
+++ client/src/init.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>WELCOME TO USER API</title>
+    <link href="" rel="stylesheet" />
+  </head>
+<body>
+    <section class="center" id="user-interface">
+        <h1>WELCOME TO USER API</h1>
+    <div id="btn">
+	<button type="button" id="allinput" >Get Users</button>
+    <input type="text" id="number-input" placeholder="Enter User ID" />
+	<button type="button" id="input">Get User By ID</button>
+    </div>
+    </section>
+
+    <div id="root"></div>
+</body>
+
+  
+<script src="../"></script>
+</html>
